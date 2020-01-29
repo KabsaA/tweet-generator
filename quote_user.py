90e0bd9f139952f18ad0b5b0471e889bba89beac
Kabsa_Abdi <KabsaAbdi23@gmail.com>
2020-01-29 11:23:17 -0800

    rearrange.py added

--- quote_user.py
+++ quote_user.py
@@ -1,4 +1,6 @@
 from python_quote import random_python_quote
+from rearrange import rearrange
 
 
-print(random_python_quote())
+#print(random_python_quote())
+print(rearrange("the cow jumped over the moon"))
---
+++ rearrange.py
@@ -0,0 +1,12 @@
+import random
+
+def rearrange(sentence):
+    words = sentence.split(" ")
+    random_sentence = []
+
+    while len(words) > 0:
+        for _ in range(len(words)):
+            random_list = random.randint(0, len(words) - 1)
+            random_sentence.append(words[random_list])
+            words.remove(words[random_list])
+    return random_sentence
